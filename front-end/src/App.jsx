9394e791600f64124abf9f87ba6a7f6a7d91543a
Jester <asoy.jestern@gmail.com>
2025-07-19 11:21:19 +0800

    intro

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import Greetings from "./common/Greetings";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello, React!</h1>
+        <Greetings />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ front-end/src/common/Greetings.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Greetings = () => {
+  return (
+    <div>
+      <p class="text-4xl font-bold text-red-500 ">This is greeting components</p>
+    </div>
+  );
+};
+
+export default Greetings;
