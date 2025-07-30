8e17c541b86d15a6b02f35bfc250952d40b12e58
Jester <asoy.jestern@gmail>
2025-07-30 19:56:30 +0800

    PLEASE TAILWIND

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css"; // or ./app.css if that's your Tailwind file
-
+import "./app.css"; // Import Tailwind CSS file
 import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
