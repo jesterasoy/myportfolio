e2351e5f5cf5956a7e001a3882ba7734170ba558
Jester <asoy.jestern@gmail>
2025-07-30 20:06:49 +0800

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./app.css"; // Import Tailwind CSS file
+import "./App.css"; // Import Tailwind CSS file
+//qweqwe
 import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
