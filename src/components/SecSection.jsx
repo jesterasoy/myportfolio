0b095324257dcef8398f9bfd5072cba6f051fcf1
Jester <asoy.jestern@gmail>
2025-07-31 14:51:48 +0800

    Updated

--- src/components/SecSection.jsx
+++ src/components/SecSection.jsx
@@ -5,6 +5,8 @@ import { gsap } from "gsap";
 
 function SecSection() {
   const titleRef = useRef(null);
+  const boxRef = useRef(null);
+  const boxRefRight = useRef(null);
   useEffect(() => {
     const tl = gsap.timeline();
 
@@ -13,30 +15,18 @@ function SecSection() {
       y: 40,
       duration: 1,
       ease: "power3.out",
-    });
-  }, []);
-  const itemsRef = useRef([]);
-
-  // Clear refs each render
-  itemsRef.current = [];
-
-  const addToRefs = (el) => {
-    if (el && !itemsRef.current.includes(el)) {
-      itemsRef.current.push(el);
-    }
-  };
-
-  useEffect(() => {
-    itemsRef.current.forEach((el, i) => {
-      gsap.from(el, {
+    }).from(
+      boxRef.current,
+      {
         opacity: 0,
-        x: i % 2 === 0 ? -50 : 50, // even from left, odd from right
+        x: 50,
         duration: 1.2,
-        delay: i * 0.2, // stagger effect
         ease: "power3.out",
-      });
-    });
+      },
+      "-=0.8"
+    );
   }, []);
+
   return (
     <>
       <div className="p-10 max-w-screen-2xl mx-auto min-h-screen">
@@ -52,56 +42,78 @@ function SecSection() {
         {/*DISPLAYING OF PROJECTS */}
         <div className="grid grid-cols-1 mt-15 sora">
           {myProjectsData.map((project, index) => (
-            <div
-              key={project.id || index}
-              ref={addToRefs}
-              className="mb-10 relative w-full flex flex-col lg:flex-row rounded-lg overflow-hidden shadow-md"
-            >
-              {index % 2 === 0 ? (
-                <>
-                  {/* Image Left */}
-                  <div
-                    className="w-full lg:w-1/2 h-[200px] lg:h-[400px]"
-                    style={{
-                      backgroundImage: `url('images/${project.Cover}')`,
-                      backgroundSize: "cover",
-                      backgroundRepeat: "no-repeat",
-                      clipPath:
-                        window.innerWidth >= 1024
-                          ? "polygon(0 0, 90% 0, 100% 100%, 0% 100%)"
-                          : "none",
-                    }}
-                  ></div>
+            <div key={project.id || index} className="mb-10">
+              <div className="relative w-full flex flex-col lg:flex-row rounded-lg overflow-hidden shadow-md">
+                {index % 2 === 0 ? (
+                  <>
+                    {/* Gradient Left (even index) */}
+                    <div
+                      className="w-full lg:w-1/2 h-[200px] lg:h-[400px]"
+                      ref={boxRef}
+                      style={{
+                        backgroundImage: `url('images/${project.Cover}')`,
+                        backgroundSize: "cover",
+                        backgroundRepeat: "no-repeat",
+                        clipPath:
+                          window.innerWidth >= 1024
+                            ? "polygon(0 0, 90% 0, 100% 100%, 0% 100%)"
+                            : "none",
+                      }}
+                    ></div>
 
-                  {/* Text Right */}
-                  <div className="w-full lg:w-1/2 bg-white lg:h-[300px] p-6 flex flex-col justify-center">
-                    <h2 className="text-2xl font-bold mb-2">{project.Title}</h2>
-                    <p className="text-gray-700">{project.Preview}</p>
-                  </div>
-                </>
-              ) : (
-                <>
-                  {/* Text Left */}
-                  <div className="w-full lg:w-1/2 bg-white lg:h-[300px] p-6 flex flex-col justify-center">
-                    <h2 className="text-2xl font-bold mb-2">{project.Title}</h2>
-                    <p className="text-gray-700">{project.Preview}</p>
-                  </div>
+                    {/* Text Right */}
+                    <div
+                      className="w-full lg:w-1/2 bg-white lg:h-[300px] p-6 flex flex-col justify-center"
+                      ref={boxRef}
+                    >
+                      <Link
+                        to={`/ProjectData/${project.Title.replace(/\s+/g, "-")
+                          .replace(/\./g, "")
+                          .toLowerCase()}`}
+                        state={{ project }}
+                        className="text-decoration-none hover:underline hover:text-yellow-600 transition-all duration-300"
+                      >
+                        <h2 className="text-2xl font-bold mb-2  ">
+                          {project.Title}
+                        </h2>
+                      </Link>
+                      <p className="text-gray-700">{project.Preview}</p>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    {/* Text Left */}
+                    <div className="w-full lg:w-1/2 bg-white lg:h-[300px] p-6 flex flex-col justify-center">
+                      <Link
+                        to={`/ProjectData/${project.Title.replace(/\s+/g, "-")
+                          .replace(/\./g, "")
+                          .toLowerCase()}`}
+                        state={{ project }}
+                        className="text-decoration-none hover:underline hover:text-yellow-600 transition-all duration-300"
+                      >
+                        <h2 className="text-2xl font-bold mb-2 truncate">
+                          {project.Title}
+                        </h2>
+                      </Link>
+                      <p className="text-gray-700">{project.Preview}</p>
+                    </div>
 
-                  {/* Image Right */}
-                  <div
-                    className="w-full lg:w-1/2 h-[200px] lg:h-[400px]"
-                    style={{
-                      backgroundImage: `url('images/${project.Cover}')`,
-                      backgroundSize: "cover",
-                      backgroundRepeat: "no-repeat",
-                      clipPath:
-                        window.innerWidth >= 1024
-                          ? "polygon(10% 0, 100% 0, 100% 100%, 0% 100%)"
-                          : "none",
-                    }}
-                  ></div>
-                </>
-              )}
+                    {/* Gradient Right (odd index) */}
+                    <div
+                      className="w-full lg:w-1/2 h-[200px] lg:h-[400px]"
+                      style={{
+                        backgroundImage: `url('images/${project.Cover}')`,
+                        backgroundSize: "cover",
+                        backgroundRepeat: "no-repeat",
+                        clipPath:
+                          window.innerWidth >= 1024
+                            ? "polygon(10% 0, 100% 0, 100% 100%, 0% 100%)"
+                            : "none",
+                      }}
+                    ></div>
+                  </>
+                )}
+              </div>
             </div>
           ))}
         </div>
