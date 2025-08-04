7fd8e9ccc7adea24e23eb607affee69164b45b8a
Jester <asoy.jestern@gmail>
2025-08-04 14:12:01 +0800

    Updated

--- src/components/ThirdSection.jsx
+++ src/components/ThirdSection.jsx
@@ -4,27 +4,32 @@ const ThirdSection = () => {
   const myCertificatesData = [
     { id: 1, Cover: "certificates/hackFest.jpg" },
     { id: 2, Cover: "certificates/dataAnalytics.jpg" },
+    { id: 3, Cover: "certificates/figma.jpg" },
+    { id: 4, Cover: "certificates/plm.jpg" },
+    { id: 5, Cover: "certificates/QA.jpg" },
+    { id: 6, Cover: "certificates/webDev.jpg" },
   ];
 
   return (
     <>
-      <div className="p-10 max-w-screen-2xl mx-auto min-h-screen -mt-10">
+      <div className="p-10 max-w-screen-2xl mx-auto h-auto -mt-10">
         <div className="sora">
           <h1 className="text-center font-bold text-4xl 2xl:text-6xl tracking-wide uppercase line-through decoration-yellow-500/40">
             Certificates
           </h1>
         </div>
-
-        <div className="grid grid-cols-4 gap-4 mt-10 2xl:grid-cols-5">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-6 mt-10">
           {myCertificatesData.map((certificate, index) => (
-            <>
-              <div className="" key={certificate.id || index}>
-                <img
-                  src={`images/${certificate.Cover}`}
-                  className="object-cover h-[auto] w-[300px] rounded-lg shadow-md"
-                />
-              </div>
-            </>
+            <div key={certificate.id || index} className="flex justify-center">
+              <img
+                src={`images/${certificate.Cover}`}
+                alt={`Certificate ${certificate.title || index + 1}`}
+                className="object-cover rounded-lg shadow-md 
+                   hover:shadow-lg hover:scale-105 
+                   transition-transform duration-300 
+                   w-full max-w-[300px] h-auto"
+              />
+            </div>
           ))}
         </div>
       </div>
